7d72a4d595bc955399e06bc31ab399f06ec54612
Harrison Booth <harrisonboothh@gmail.com>
2020-11-02 10:25:34 +0000

    Make rollDie test a little more encompassing

--- tests/die_spec.js
+++ tests/die_spec.js
@@ -3,7 +3,10 @@ const rollDie = require('../models/die')
 
 suite('Die function', () => {
   test('should roll between a 1 and the number passed', () => {
-    const roll = rollDie(6)
-    assert(roll >= 1 && roll <= 6)
+    const upperLimits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 30, 560, 9999]
+    upperLimits.forEach(upperLimit => {
+      const roll = rollDie(upperLimit)
+      assert(roll >= 1 && roll <= upperLimit)
+    })
   })
 })
